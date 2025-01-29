c0deec5b40cab754045419e2beb282daf9ff663e
Peterson Prestes Carneiro <peters.prestes@gmail.com>
2025-01-29 14:49:37 -0300

    feat: create UserService class

---
+++ gerencimantodeprocessos/src/main/java/com/gerencimantodeprocessos/service/UserService.java
@@ -0,0 +1,83 @@
+package com.gerencimantodeprocessos.service;
+
+import com.gerencimantodeprocessos.domain.user.User;
+import com.gerencimantodeprocessos.dtos.UserDTO;
+import com.gerencimantodeprocessos.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    //metedos para salvar,listar,atualizar e deletar
+    //também outras regras de negocio
+
+    public UserDTO saveUser(UserDTO userDTO){
+        User user = new User(userDTO);
+        User newUser = userRepository.save(user);
+        return new UserDTO(
+                newUser.getId(),
+                newUser.getFirstName(),
+                newUser.getLastName(),
+                newUser.getLogin(),
+                newUser.getPassword(),
+                newUser.getUserType()
+        );
+    }
+
+    public List<UserDTO> listUser(){
+        return userRepository.findAll().stream().map(user ->
+                new UserDTO(
+                        user.getId(),
+                        user.getFirstName(),
+                        user.getLastName(),
+                        user.getLogin(),
+                        user.getPassword(),
+                        user.getUserType()
+                )
+        ).toList();
+    }
+
+    public Optional<UserDTO> findByIdUser(String id){
+        return userRepository.findById(id).map(user ->
+                new UserDTO(
+                        user.getId(),
+                        user.getFirstName(),
+                        user.getLastName(),
+                        user.getLogin(),
+                        user.getPassword(),
+                        user.getUserType())
+        );
+    }
+
+    public UserDTO updateUser(String id, UserDTO userDTO){
+        return userRepository.findById(id).map(user -> {
+            user.setFirstName(userDTO.firstName());
+            user.setLastName(userDTO.lastName());
+            user.setLogin(userDTO.login());
+            user.setPassword(userDTO.password());
+            user.setUserType(userDTO.userType());
+            User updateUser = userRepository.save(user);
+            return new UserDTO(
+                    updateUser.getId(),
+                    updateUser.getFirstName(),
+                    updateUser.getLastName(),
+                    updateUser.getLogin(),
+                    updateUser.getPassword(),
+                    updateUser.getUserType()
+            );
+        }).orElseThrow(()-> new RuntimeException("User not found"));
+    }
+
+    public void deleteUser(String id){
+        userRepository.deleteById(id);
+    }
+
+
+}
