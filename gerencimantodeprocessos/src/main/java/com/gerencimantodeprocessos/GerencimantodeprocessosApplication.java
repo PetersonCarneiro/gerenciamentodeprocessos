a133626e07533285b8c214cf85e319ea7c826cf5
Peterson Prestes Carneiro <peters.prestes@gmail.com>
2025-01-29 14:46:25 -0300

    feat: initial config

---
+++ gerencimantodeprocessos/src/main/java/com/gerencimantodeprocessos/GerencimantodeprocessosApplication.java
@@ -0,0 +1,14 @@
+package com.gerencimantodeprocessos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerencimantodeprocessosApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(GerencimantodeprocessosApplication.class, args);
+	}
+
+}
