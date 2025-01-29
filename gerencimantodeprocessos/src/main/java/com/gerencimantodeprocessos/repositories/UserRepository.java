66c9e1688c789014032fca045a74a9dd8e58b7dc
Peterson Prestes Carneiro <peters.prestes@gmail.com>
2025-01-29 14:49:18 -0300

    feat: create UserRepository class

---
+++ gerencimantodeprocessos/src/main/java/com/gerencimantodeprocessos/repositories/UserRepository.java
@@ -0,0 +1,15 @@
+package com.gerencimantodeprocessos.repositories;
+
+import com.gerencimantodeprocessos.domain.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, String> {
+
+    List<User> findByFirstName(String firstName);
+    Optional<User> findByLogin(String login);
+}
